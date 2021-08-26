3fecad808792ba2b8344925019d8581bb95fad7d
Kaz Sato <kazunori279@gmail.com>
2021-08-27 08:03:00 +0900

    Added Firebase support

--- running.ino
+++ running.ino
@@ -1,21 +1,39 @@
 /*
  * running
  * 
- * Prerequisite:
- * - M5Stack
- * - PbHub: https://docs.m5stack.com/en/unit/pbhub copy porthub.cpp and porthub.h to the sketch directory
+ * Requirements:
+ * - M5Stack lib
+ * - PbHub: https://docs.m5stack.com/en/unit/pbhub copy porthub.cpp and porthub.h
+ * - FirebaseESP32 lib
+ * - running-config.h: create a file with the following configurations
+ *   #define WIFI_SSID "<SSID>"
+ *   #define WIFI_PASS "<PASSWORD>"
+ *   #define FIREBASE_HOST "<FIREBASE HOST NAME>"
+ *   #define FIREBASE_AUTH "<FIREBASE AUTH KEY>"
 */
+#include <time.h>
 #include <M5Stack.h>
 #include <Wire.h>
+#include <FirebaseESP32.h>
 #include "porthub.h"
+#include "running_config.h"
 
-//#define HR_SIGNAL_INPUT 26 // pin no of the heart rate sensor input
-//#define TM_SPEED_INPUT 22  // pin no of the treadmill line sensor input
-#define HR_SIGNALS_SIZE 10 // how many heart beats for calculating a heart rate
+#define HR_WINDOW_SIZE 10 // how many heart beats for calculating a heart rate
 
-#define REMOTE_SPEED_UP 17 // pin no for increasing the speed
+#define FIREBASE_ROOT "gcp-running"
+
+#define NTP_HOST "ntp.nict.jp" // NTP host for clock correction
+#define TIMEZONE 60 * 60 * 9 // Timezone offset (JST = 9) for clock correction
+
+#define REMOTE_SPEED_UP 17   // pin no for increasing the speed
 #define REMOTE_SPEED_START 2 // pin no for start/stop
-#define REMOTE_SPEED_DOWN 5 // pin no for decreasing the speed
+#define REMOTE_SPEED_DOWN 5  // pin no for decreasing the speed
+
+#define PREF_RUNNING "running" // the key for storing Preferenes
+
+FirebaseData fbdo;
+
+TFT_eSprite sprite = TFT_eSprite(&M5.Lcd);
 
 PortHub porthub;
 
@@ -24,11 +42,11 @@ void setup(){
   // init M5  
   M5.begin();
   M5.Power.begin();
+  M5.Speaker.begin();
+  M5.Speaker.setVolume(2);
 
   // init pins
   porthub.begin();
-//  pinMode(HR_SIGNAL_INPUT, INPUT);
-//  pinMode(TM_SPEED_INPUT, INPUT);
   pinMode(REMOTE_SPEED_UP, OUTPUT_OPEN_DRAIN);
   pinMode(REMOTE_SPEED_START, OUTPUT_OPEN_DRAIN);
   pinMode(REMOTE_SPEED_DOWN, OUTPUT_OPEN_DRAIN);
@@ -37,8 +55,44 @@ void setup(){
   digitalWrite(REMOTE_SPEED_DOWN, true);
 
   // init UI
-  M5.Lcd.setTextSize(3);
+  sprite.setColorDepth(8);
+  sprite.createSprite(M5.Lcd.width(), M5.Lcd.height());
+  
+  // init WiFi
+  M5.Lcd.print("WiFi.");  
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    M5.Lcd.print(".");
+  }
+  M5.Lcd.fillScreen(BLACK);
+
+  // init clock
+  configTime(TIMEZONE, 0, NTP_HOST);
+
+  // init Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(50, 100);
+
+  // init finished
+  beep();
 }
+
+void beep() {
+  M5.Speaker.beep();
+  delay(100);
+  M5.Speaker.mute();
+}
+
+// UI status
+#define ST_STOP  "STOP"
+#define ST_RUN   "RUN"
+#define ST_PAUSE "PAUSE"
+String uiStatus = ST_STOP;
+String runId = "";
+unsigned long runStartedAt = 0;
 
 // the main loop
 void loop() {
@@ -49,28 +103,46 @@ void loop() {
   // read treadmill speed 
   readTreadmillSpeed();
 
+  // send the metrics to Firebase
+  sendMetrics();
+
+  // read buttons
   M5.update();
   if (M5.BtnA.wasPressed()) {
-    digitalWrite(REMOTE_SPEED_UP, false);
-    delay(500);
-    digitalWrite(REMOTE_SPEED_UP, true);
+    if (uiStatus == ST_STOP) {
+      uiStatus = ST_RUN;
+      runId = getRunId();
+      runStartedAt = millis();
+      beep();
+    }
   }
-
   if (M5.BtnB.wasPressed()) {
-    digitalWrite(REMOTE_SPEED_START, false);
-    delay(500);
-    digitalWrite(REMOTE_SPEED_START, true);
+    if (uiStatus == ST_RUN || uiStatus == ST_PAUSE) {
+      uiStatus = ST_STOP;
+      beep();
+    }    
   }
-
   if (M5.BtnC.wasPressed()) {
-    digitalWrite(REMOTE_SPEED_DOWN, false);
-    delay(500);
-    digitalWrite(REMOTE_SPEED_DOWN, true);
+    if (uiStatus == ST_RUN) {
+      uiStatus = ST_PAUSE;
+    } else if (uiStatus == ST_PAUSE) {
+      uiStatus = ST_RUN;
+    }
+    beep();
   }
 
   // update UI
   updateUI();
-  delay(10);
+//  delay(10);
+}
+
+String getRunId() {
+  char buf[20];
+  struct tm ti;
+  getLocalTime(&ti);
+  sprintf(buf, "%04d%02d%02d-%02d%02d%02d", 
+    ti.tm_year + 1900, ti.tm_mon + 1, ti.tm_mday, ti.tm_hour, ti.tm_min, ti.tm_sec); 
+  return "R" + (String)buf;
 }
 
 // Heart rate pubic status
@@ -78,38 +150,46 @@ boolean hrSignal = false;
 float heartRate = 0;
 
 // Heart rate private status
-int hrIdx = 0;
 boolean lastHrSignal = false;
-unsigned long lastHrTime = 0;
-unsigned long hrSignals[HR_SIGNALS_SIZE];
+unsigned long lastHrSignalAt = 0;
+float heartRates[HR_WINDOW_SIZE];
+int hrCount = 0;
 
 void readHeartRate() {
-  unsigned long beatTime;
+
+  // read HR signal
+  unsigned long now = millis();
   hrSignal = (boolean) porthub.hub_d_read_value_A(HUB1_ADDR);
-//  hrSignal = (boolean) digitalRead(HR_SIGNAL_INPUT);
   if (hrSignal != lastHrSignal) {
     lastHrSignal = hrSignal;
-    unsigned long now = millis();
-    if (hrSignal == true && (now - lastHrTime) > 200) { // remove signals shorter than 200 ms
-      addBeatTime(now);
-      lastHrTime = now;
-    }
-    heartRate = calcHr();
-  }  
-}
 
-void addBeatTime(unsigned long beatTime) {
-  hrSignals[hrIdx++] = beatTime;
-  if (hrIdx == HR_SIGNALS_SIZE) {
-    hrIdx = 0;
+    // update HR, removing signals shorter than 400 ms (150/min)
+    if (hrSignal == true && (now - lastHrSignalAt) > 400) { 
+      float hr = 60.0 * 1000.0 / float(now - lastHrSignalAt);
+      Serial.println(hr);
+      heartRate = addAndCalcHeartRate(hr);
+      lastHrSignalAt = now;
+    }
+
+  }  
+
+  // set HR to 0 when hrSignal is not coming for 1.5 sec
+  if (lastHrSignalAt == 0 || (now - lastHrSignalAt) > 1500) {
+    heartRate = 0;
+    hrCount = 0;
   }
 }
 
-float calcHr() {
-  unsigned long lastHr = hrIdx == 0 ? hrSignals[HR_SIGNALS_SIZE - 1] : hrSignals[hrIdx - 1];
-  unsigned long firstHr = hrSignals[hrIdx];
-  float hrInterval = float(lastHr - firstHr) / float(HR_SIGNALS_SIZE - 1);
-  return firstHr == 0 ? -1.0 : float(1000.0 / hrInterval * 60.0);
+float addAndCalcHeartRate(float hr) {
+  heartRates[hrCount++ % HR_WINDOW_SIZE] = hr;
+  if (hrCount < HR_WINDOW_SIZE + 5) {
+    return 0; // return 0 if it's too few
+  }
+  float sum = 0;
+  for (int i = 0; i < HR_WINDOW_SIZE; i++) {
+    sum += heartRates[i];
+  }
+  return sum / float(HR_WINDOW_SIZE);
 }
 
 // Treadmill speed public status
@@ -118,21 +198,28 @@ float treadmillSpeed = 0;
 
 // Treadmill speed private status
 boolean lastSpSignal = false;
-unsigned long lastSpSignalTime = 0;
+unsigned long lastSpSignalAt = 0;
 float spSignalInterval = 0;
 
 void readTreadmillSpeed() {
-//  spSignal = !digitalRead(TM_SPEED_INPUT);
+  
+  unsigned long now = millis();
   spSignal = !(boolean)porthub.hub_d_read_value_A(HUB2_ADDR);
   if (spSignal != lastSpSignal) {
     lastSpSignal = spSignal;
-    unsigned long now = millis();
-    if (spSignal && (now - lastSpSignalTime) > 500) { // remove signals shorter than 500 ms
-      spSignalInterval = float(now - lastSpSignalTime);
+
+    // update treadmill speed, removing signals shorter than 500 ms
+    if (spSignal && (now - lastSpSignalAt) > 500) {
+      spSignalInterval = float(now - lastSpSignalAt);
       treadmillSpeed = calcSpeed(spSignalInterval);
-      lastSpSignalTime = now;
+      lastSpSignalAt = now;
     }
-  }  
+  }
+
+  // set speed to 0 when the signal is not coming for 10 sec
+  if (lastSpSignalAt == 0 || (now - lastSpSignalAt) > 10000) {
+    treadmillSpeed = 0;
+  }
 }
 
 // calculate the speed from the interval by a power series function
@@ -141,11 +228,28 @@ float calcSpeed(float interval) {
   return 11716.0 * pow(interval, -1.04); 
 }
 
-void updateUI() {
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(50, 100);
-  M5.Lcd.print("HR: " + String(hrSignal) + ", " + String(heartRate));  
-  M5.Lcd.setCursor(50, 140);
-  M5.Lcd.print("TM: " + String(spSignal) + ", " + String(treadmillSpeed));
+unsigned long lastMetricsSentAt = 0;
+
+void sendMetrics() {
+  unsigned long now = millis();
+  if (uiStatus == ST_RUN && now - lastMetricsSentAt > 1000) {
+    lastMetricsSentAt = now;
+    String elapsedTime = String(int(now - runStartedAt) / 1000);
+    String path = "/" + String(FIREBASE_ROOT) + "/" + runId + "/" + elapsedTime + "/";
+    Firebase.setFloatAsync(fbdo, path + "hr", heartRate);
+    Firebase.setFloatAsync(fbdo, path + "speed", treadmillSpeed);
+    Serial.println(path);
+  }
 }
 
+void updateUI() {
+  sprite.setTextSize(3);
+  sprite.fillScreen(BLACK);
+  sprite.setCursor(50, 50);
+  sprite.print("Status: " + uiStatus);  
+  sprite.setCursor(50, 100);
+  sprite.print("HR: " + String(hrSignal) + ", " + String(heartRate));  
+  sprite.setCursor(50, 140);
+  sprite.print("TM: " + String(spSignal) + ", " + String(treadmillSpeed));
+  sprite.pushSprite(0, 0);
+}
